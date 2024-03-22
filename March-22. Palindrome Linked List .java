c32c3d3533708b60f7fc7ae8102d4c660f2b3001
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-03-22 05:47:20 +0530

    22-march

---
+++ March-22. Palindrome Linked List .java
@@ -0,0 +1,29 @@
+March-22. Palindrome Linked List :
+234.
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head, prev, temp;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+        while (slow != null) {
+            temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        fast = head;
+        slow = prev;
+        while (slow != null) {
+            if (fast.val != slow.val) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
