b2538f40790fda430714a5564c1cc63789324812
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-03-29 10:04:57 +0530

    29-mar

---
+++ March-29. Count Subarrays Where Max Element Appears at Least K Times.java
@@ -0,0 +1,23 @@
+March-29. Count Subarrays Where Max Element Appears at Least K Times :
+2962.
+
+
+class Solution {
+    public long countSubarrays(int[] nums, int k) {
+        int mx = Arrays.stream(nums).max().getAsInt();
+        long ans = 0;
+        int l = 0,r=0,n=nums.length;
+        
+        while (r < n) {
+            k -= nums[r] == mx ? 1 : 0;
+            r++;
+            while (k == 0) {
+                k += nums[l] == mx ? 1 : 0;
+                l++;
+            }
+            ans += l;
+        }
+        
+        return ans;
+    }
+}
