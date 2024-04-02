94726c0661057b2fe5331ad1ba668b4a9df16059
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-04-02 05:56:40 +0530

    april 2

---
+++ April 2024/April-02.Isomorphic Strings.java
@@ -0,0 +1,24 @@
+April-02.Isomorphic Strings :
+205.
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        int map1[]=new int[200];
+        int map2[]=new int[200];
+
+        if(s.length()!=t.length())
+            return false;
+
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+                return false;
+
+            map1[s.charAt(i)]=i+1;
+            map2[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
+}
