74df9b591a656d02b3cba5457e7618bfa7b86df0
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-04-08 10:59:31 +0530

    8-april

---
+++ April 2024/April-8.Number of Students Unable to Eat Lunch.java
@@ -0,0 +1,20 @@
+April-8.Number of Students Unable to Eat Lunch
+1700. 
+
+
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] counts = new int[2];
+        for (int student : students) counts[student]++;
+        
+        int remaining = sandwiches.length;
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0) break;
+            if (remaining-- == 0) break;
+            counts[sandwich]--;
+        }
+        
+        return remaining;
+    }
+}
