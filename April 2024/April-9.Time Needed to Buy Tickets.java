62795a0f8794c962f5b7bb3b7f9ece75deab5e51
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-04-09 06:06:40 +0530

    9-april

---
+++ April 2024/April-9.Time Needed to Buy Tickets.java
@@ -0,0 +1,21 @@
+April-9.Time Needed to Buy Tickets :
+2073.
+
+
+
+
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int total = 0;
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                total += Math.min(tickets[i], tickets[k]);
+            } else {
+                total += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return total;
+    }
+}
