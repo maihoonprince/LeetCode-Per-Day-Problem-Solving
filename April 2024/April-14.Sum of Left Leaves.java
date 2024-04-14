4d1026ec40551d748a0fbe51646b51fc42d85da0
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-04-14 08:55:46 +0530

    14 april

---
+++ April 2024/April-14.Sum of Left Leaves.java
@@ -0,0 +1,24 @@
+April-14.Sum of Left Leaves:
+404.
+
+
+
+class Solution {
+    int leftLeafSum = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        helper(root);
+        return leftLeafSum;
+    }
+    private void helper(TreeNode root){
+        if(root == null) return;
+        if(isLeafNode(root.left)) leftLeafSum += root.left.val;
+        helper(root.left);
+        helper(root.right);
+    }
+    private boolean isLeafNode(TreeNode root){
+        if(root == null) return false;
+        if(root.left == null && root.right == null) return true;
+
+        return false;
+    }
+}
