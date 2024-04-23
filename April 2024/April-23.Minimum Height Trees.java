9fae291c1fbb2a278c87564a576d03d411988f01
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-04-23 06:09:04 +0530

    23-april

---
+++ April 2024/April-23.Minimum Height Trees.java
@@ -0,0 +1,53 @@
+April-23.Minimum Height Trees :
+310.
+
+
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> result = new ArrayList<>();
+        if (n == 1) {
+            result.add(0);
+            return result;
+        }
+        
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        
+        int[] degree = new int[n];
+        
+        for (int[] edge : edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+        }
+        
+        Queue<Integer> leaves = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (degree[i] == 1) {
+                leaves.offer(i);
+            }
+        }
+        
+        while (n > 2) {
+            int size = leaves.size();
+            n -= size;
+            for (int i = 0; i < size; i++) {
+                int leaf = leaves.poll();
+                for (int neighbor : adjList.get(leaf)) {
+                    if (--degree[neighbor] == 1) {
+                        leaves.offer(neighbor);
+                    }
+                }
+            }
+        }
+        
+        result.addAll(leaves);
+        return result;
+    }
+}
