cf90e069f9469dce425e755b132a8d45ee7aa8f5
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-05-16 06:22:24 +0530

    may-16

---
+++ May 2024/May-16. Evaluate Boolean Binary Tree.java
@@ -0,0 +1,39 @@
+May-16. Evaluate Boolean Binary Tree :
+2331.
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean helper(TreeNode root) {
+        if(root.val == 0 || root.val == 1) {
+            return root.val == 1;
+        } else if(root.val == 2) {
+            return helper(root.left) || helper(root.right);
+        } else if(root.val == 3) {
+            return helper(root.left) && helper(root.right);
+        } 
+
+        return false;
+    }
+    public boolean evaluateTree(TreeNode root) {
+        if(root == null) {
+            return false;
+        }
+
+        return helper(root);
+    }
+}
