b785d164e3ff389e8132f2bc4c440d795906b3c8
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-05-23 12:33:38 +0530

    May-23

---
+++ May 2024/May-23. The Number of Beautiful Subsets.java
@@ -0,0 +1,29 @@
+May-23. The Number of Beautiful Subsets :
+2597.
+
+
+class Solution {
+    public int dfs(int[] nums, int idx, int k, HashMap<Integer, Integer> mp) {
+        if (idx == nums.length) return 1;
+
+        int taken = 0;
+        if (!mp.containsKey(nums[idx] - k) && !mp.containsKey(nums[idx] + k)) {
+            mp.put(nums[idx], mp.getOrDefault(nums[idx], 0) + 1);
+            taken = dfs(nums, idx + 1, k, mp);
+            mp.put(nums[idx], mp.get(nums[idx]) - 1);
+            if (mp.get(nums[idx]) == 0) {
+                mp.remove(nums[idx]);
+            }
+        }
+        
+        int notTaken = dfs(nums, idx + 1, k, mp);
+        
+        return taken + notTaken;
+    }
+
+    public int beautifulSubsets(int[] nums, int k) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int ans = dfs(nums, 0, k, mp);
+        return ans - 1;
+    }
+}
