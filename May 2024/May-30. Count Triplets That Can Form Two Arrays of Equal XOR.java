a95d90ca663a65a81bfcdbbbbe08365dafa0a1f4
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-05-30 05:57:01 +0530

    may-30

---
+++ May 2024/May-30. Count Triplets That Can Form Two Arrays of Equal XOR.java
@@ -0,0 +1,28 @@
+May-30. Count Triplets That Can Form Two Arrays of Equal XOR :
+1442.
+
+
+
+
+
+class Solution {
+    public int countTriplets(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n + 1];
+        
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] ^ arr[i];
+        }
+        
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int k = i + 1; k < n; k++) {
+                if (prefix[i] == prefix[k + 1]) {
+                    count += (k - i);
+                }
+            }
+        }
+        
+        return count;
+    }
+}
