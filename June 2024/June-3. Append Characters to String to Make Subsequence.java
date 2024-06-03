953cd7389b376a99125a06063bd25a4591e5ba4f
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-06-03 05:38:22 +0530

    june 3

---
+++ June 2024/June-3. Append Characters to String to Make Subsequence.java
@@ -0,0 +1,22 @@
+June-3. Append Characters to String to Make Subsequence :
+2486.
+
+
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i=0;
+        int j=0;
+        int c=0;
+        while(i<s.length()&&j<t.length())
+        {
+            if(s.charAt(i)==t.charAt(j))
+            {
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return t.length()-j;
+    }
+}
