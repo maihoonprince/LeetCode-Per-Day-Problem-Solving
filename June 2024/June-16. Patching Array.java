5a76c1fbacca9a979f22d65f228dd63a60be3b19
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-06-16 07:37:10 +0530

    june-16

---
+++ June 2024/June-16. Patching Array.java
@@ -0,0 +1,23 @@
+June-16. Patching Array :
+330.
+
+
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int result = 0;
+        int i = 0;
+
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss += miss;
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
