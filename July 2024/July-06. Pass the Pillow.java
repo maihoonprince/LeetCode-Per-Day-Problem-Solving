14fbada8fbbe68c099c95047171e9c265d4894e2
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2024-07-06 23:21:42 +0530

    july-06

---
+++ July 2024/July-06. Pass the Pillow.java
@@ -0,0 +1,10 @@
+July-06. Pass the Pillow :
+2582.
+
+
+class Solution {
+    public int passThePillow(int n, int time) {
+        int chunks = time / (n - 1);
+        return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+    }
+}
